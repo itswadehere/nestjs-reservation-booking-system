3b47f472f34642a0ac69924927d3d4621a19dbc6
itswadehere <itswade.dev@gmail.com>
2025-07-25 14:27:50 +0700

    Abstract schema and repository

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,61 @@
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
+import { AbstractDocument } from './abstract.schema';
+import { Logger, NotFoundException } from '@nestjs/common';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    // By default, Mongo will return a hydrated document, which is a document with a bunch of internal mongoose properties
+    // and helper methods on it
+    const document = await this.model
+      .findOne(filterQuery)
+      .lean<TDocument>(true); // -> lean to get the plain JavaScript object
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document was not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, {
+        new: true, // -> return the object after the `update` was applied
+      })
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document was not found');
+    }
+
+    return document;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery).lean<TDocument[]>(true);
+  }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    return this.model.findOneAndDelete(filterQuery).lean<TDocument>(true);
+  }
+}
